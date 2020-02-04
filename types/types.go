325ef17a5820f6ac5b306329ef6ffe3fd3680e9e
neville bokdawalla <nevillekb@gmail.com>
2020-02-05 01:16:51 +0530

    added code to demonstrate the bool type

---
+++ types/types.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// bool
+	success := true
+	completed := false
+	fmt.Println("success is", success, "and completed is", completed)
+
+	// signed integers
+
+	// unsigned integers
+
+	// floating points
+
+	// complex
+
+	// byte
+
+	// rune
+
+	// string
+
+	// type conversion
+}
